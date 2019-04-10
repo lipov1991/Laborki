9fc493663869574584afde33af15e72b98e23c2a
lipov <klipka1991@gmail.com>
2019-04-10 22:50:53 +0200

    lab1

--- Laborki/app/src/main/java/pl/krzysztof/lipka/laborki/main/MainActivity.kt
+++ Laborki/app/src/main/java/pl/krzysztof/lipka/laborki/main/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : MvpActivity<MainView, MainPresenter>(), MainView {
     override fun onCreate(
         savedInstanceState: Bundle?
     ) {
+        // TODO lab1
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setUpActionBar()
