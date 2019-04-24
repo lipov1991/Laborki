c479a43bd1aee76ff15ffb94050fb7c8273ecc75
klipka@n7mobile.com <klipka@n7mobile.com>
2019-04-24 18:31:25 +0200

    Remove unnecessary files

--- Laborki/app/src/main/java/pl/krzysztof/lipka/laborki/di/AppModule.kt
+++ Laborki/app/src/main/java/pl/krzysztof/lipka/laborki/di/AppModule.kt
@@ -8,8 +8,6 @@ import dagger.android.support.AndroidSupportInjectionModule
 import pl.krzysztof.lipka.laborki.App
 import pl.krzysztof.lipka.laborki.common.utils.alert_dialog.AlertDialogUtils
 import pl.krzysztof.lipka.laborki.common.utils.alert_dialog.AlertDialogUtilsImpl
-import pl.krzysztof.lipka.laborki.data.CoordinatesRepository
-import pl.krzysztof.lipka.laborki.data.CoordinatesRepositoryImpl
 
 @Module(includes = [AndroidSupportInjectionModule::class, ActivityModule::class])
 abstract class AppModule {
@@ -28,9 +26,4 @@ abstract class AppModule {
     abstract fun bindsAlertDialogUtils(
         alertDialogUtilsImpl: AlertDialogUtilsImpl
     ): AlertDialogUtils
-
-    @Binds
-    abstract fun bindsCoordinatesRepository(
-        coordinatesRepositoryImpl: CoordinatesRepositoryImpl
-    ): CoordinatesRepository
 }
--- Laborki/app/src/main/java/pl/krzysztof/lipka/laborki/main/recipient_data/RecipientDataPresenter.kt
+++ Laborki/app/src/main/java/pl/krzysztof/lipka/laborki/main/recipient_data/RecipientDataPresenter.kt
@@ -1,11 +1,9 @@
 package pl.krzysztof.lipka.laborki.main.recipient_data
 
-import pl.krzysztof.lipka.laborki.data.CoordinatesRepository
 import javax.inject.Inject
 
 class RecipientDataPresenter @Inject constructor(
-    private val view: RecipientDataView,
-    private val repository: CoordinatesRepository
+    private val view: RecipientDataView
 ) {
 
     // TODO
