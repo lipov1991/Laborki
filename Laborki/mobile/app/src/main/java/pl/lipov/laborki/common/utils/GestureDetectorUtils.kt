3ff73442ba9b0343ae281948f92517f0e3f4f644
lipov <klipka1991@gmail.com>
2020-02-26 19:46:26 +0100

    Initial commit

--- Laborki/mobile/app/src/main/java/pl/lipov/laborki/common/utils/GestureDetectorUtils.kt
+++ Laborki/mobile/app/src/main/java/pl/lipov/laborki/common/utils/GestureDetectorUtils.kt
@@ -3,22 +3,13 @@ package pl.lipov.laborki.common.utils
 import android.app.Activity
 import android.view.GestureDetector
 import android.view.MotionEvent
-import android.view.View
 import androidx.lifecycle.MutableLiveData
 import pl.lipov.laborki.data.model.Event
 
-class GestureDetectorUtils : GestureDetector.SimpleOnGestureListener(), View.OnTouchListener {
+class GestureDetectorUtils : GestureDetector.SimpleOnGestureListener() {
 
     val onEvent = MutableLiveData<Event>()
     private var gestureDetector: GestureDetector? = null
-
-    override fun onTouch(
-        view: View,
-        event: MotionEvent
-    ): Boolean {
-        view.performClick()
-        return false
-    }
 
     fun initGestureDetector(
         activity: Activity
