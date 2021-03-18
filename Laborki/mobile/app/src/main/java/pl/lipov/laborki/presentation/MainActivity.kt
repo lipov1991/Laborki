800d5ddd15afe40f75be92b5969a8e10ce1d9ed4
Jacek Chojnacki <jacek.0411@gmail.com>
2021-03-18 09:12:17 +0100

    lab_1

--- Laborki/mobile/app/src/main/java/pl/lipov/laborki/presentation/MainActivity.kt
+++ Laborki/mobile/app/src/main/java/pl/lipov/laborki/presentation/MainActivity.kt
@@ -1,32 +1,106 @@
 package pl.lipov.laborki.presentation
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import pl.lipov.laborki.R
 import pl.lipov.laborki.databinding.ActivityMainBinding
+import pl.lipov.laborki.presentation.login.LoginCallback
+import pl.lipov.laborki.presentation.login.LoginFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener, LoginCallback {
 
-    private val viewModel: MainViewModel by viewModel()
+    //    private val viewModel: MainViewModel by viewModel()
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mDetector: GestureDetectorCompat
+    private lateinit var mSensorManager:SensorManager
+    private lateinit var mAccelerometer :Sensor
 
-    override fun onCreate(
-        savedInstanceState: Bundle?
-    ) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewModel.run {
-            onAccelerometerNotDetected.observe(this@MainActivity) {
-                binding.info.text = getString(R.string.no_accelerometer_detected)
-            }
-            onGestureEvent.observe(this@MainActivity) {
-                binding.info.text = it.name
-            }
-            onSensorEvent.observe(this@MainActivity) {
-                binding.info.text = it.name
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, LoginFragment()).commit()
+        mDetector = GestureDetectorCompat(this, MyGestureListener(this))
+
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) == null) {
+            Toast.makeText(this, "No accelerometer detected!", Toast.LENGTH_SHORT).show()
+        }
+//        viewModel.run {
+//            onAccelerometerNotDetected.observe(this@MainActivity) {
+//                binding.info.text = getString(R.string.no_accelerometer_detected)
+//            }
+//            onGestureEvent.observe(this@MainActivity) {
+//                binding.info.text = it.name
+//            }
+//            onSensorEvent.observe(this@MainActivity) {
+//                binding.info.text = it.name
+//            }
+//        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null) {
+//            val sensorName: String = event?.sensor!!.getName();
+//            Log.d("Sensor",sensorName + ": X: " + event.values[0] + "; Y: " + event.values[1] + "; Z: " + event.values[2] + ";")
+            if (event.values[0] > 5.0 && event.values[1] > 5.0) {
+                Toast.makeText(this, "Acceleration changed!", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mSensorManager.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        mDetector.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
+
+    private class MyGestureListener(private val context: Context) : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onDown(e: MotionEvent?): Boolean {
+//            Log.d("Gestures", "onDown: $e")
+            return true
+        }
+
+        override fun onDoubleTap(e: MotionEvent?): Boolean {
+//            Log.d("Gestures", "onDoubleTap: $e")
+            Toast.makeText(context, "Double tap detected!", Toast.LENGTH_SHORT).show()
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent?) {
+//            Log.d("Gestures", "onLongPress: $e")
+            Toast.makeText(context, "Long press detected!", Toast.LENGTH_SHORT).show()
+            super.onLongPress(e)
+        }
+    }
+
+    override fun onLoginSuccess() {
+        Toast.makeText(this, "Login success", Toast.LENGTH_LONG).show()
     }
 }
