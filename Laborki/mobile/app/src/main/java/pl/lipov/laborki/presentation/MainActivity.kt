dc788167a1b1879aab56ac6f3a9f0e15de89b7d1
Emil Moryc <73442450+M-Emil@users.noreply.github.com>
2021-03-25 19:25:31 +0100

    Zadanie 2 - logowanie

--- Laborki/mobile/app/src/main/java/pl/lipov/laborki/presentation/MainActivity.kt
+++ Laborki/mobile/app/src/main/java/pl/lipov/laborki/presentation/MainActivity.kt
@@ -1,59 +1,61 @@
 package pl.lipov.laborki.presentation
 
-import android.content.Context
-import android.hardware.*
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GestureDetectorCompat
-import androidx.lifecycle.observe
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import pl.lipov.laborki.R
-import pl.lipov.laborki.common.utils.SensorEventsUtils
+import pl.lipov.laborki.data.model.Event
 import pl.lipov.laborki.databinding.ActivityMainBinding
 import pl.lipov.laborki.presentation.login.LoginCallback
 import pl.lipov.laborki.presentation.login.LoginFragment
 
-
 private const val DEBUG_TAG = "Gestures"
 
-class MainActivity() : AppCompatActivity(), GestureDetector.OnGestureListener,GestureDetector.OnDoubleTapListener, LoginCallback, SensorEventListener
+class MainActivity() : AppCompatActivity(), GestureDetector.OnGestureListener,GestureDetector.OnDoubleTapListener,
+    LoginCallback, SensorEventListener
 {
 
     private val viewModel: MainViewModel by viewModel()
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var mDetector: GestureDetectorCompat
-
-
+    lateinit var mDetector: GestureDetectorCompat
     private lateinit var sensorManager: SensorManager
 
+    val loginKeys = mutableListOf<String>()
+    var counter: Int = 0
 
 
     override fun onCreate(
         savedInstanceState: Bundle?
     ) {
+
+
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
+
+        setUpSensorStuff()
 
 
         mDetector = GestureDetectorCompat(this, this)
 
         mDetector.setOnDoubleTapListener(this)
 
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, LoginFragment())
-            .addToBackStack(null)
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(R.anim.fade_in,R.anim.fade_out)
+            .replace(R.id.fragment_container, LoginFragment())
             .commit()
-
-        setUpSensorStuff()
-
     }
 
     override fun onLoginSuccess() {
@@ -67,6 +69,46 @@ class MainActivity() : AppCompatActivity(), GestureDetector.OnGestureListener,Ge
             super.onTouchEvent(event)
         }
     }
+
+    override fun onLongPress(event: MotionEvent) {
+        Log.d(DEBUG_TAG, "onLongPress: $event")
+        loginKeys.add(Event.LONG_CLICK.toString())
+
+        counter = counter?.plus(1)
+        counter.toString()
+        Toast.makeText(this, counter.toString().plus("/4"), Toast.LENGTH_LONG).show()
+
+    }
+    override fun onDoubleTap(event: MotionEvent): Boolean {
+        Log.d(DEBUG_TAG, "onDoubleTap: $event")
+        loginKeys.add(Event.DOUBLE_TAP.toString())
+        counter = counter?.plus(1)
+        Toast.makeText(this, counter.toString().plus("/4"), Toast.LENGTH_LONG).show()
+
+
+
+        return true
+    }
+
+    override fun onSensorChanged(event: SensorEvent?)
+    {
+        if (event != null)
+        {
+            if (event.values[0] > 5 &&  event.values[1] > 5)
+            {
+                Toast.makeText(this, "ACCELERATION_CHANGE", Toast.LENGTH_LONG).show()
+                loginKeys.add(Event.ACCELERATION_CHANGE.toString())
+                counter = counter?.plus(1)
+                Toast.makeText(this, counter.toString().plus("/4"), Toast.LENGTH_LONG).show()
+
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "Nie wykryto akcelerometra", Toast.LENGTH_LONG).show()
+        }
+   }
+
 
     override fun onDown(event: MotionEvent): Boolean {
         Log.d(DEBUG_TAG, "onDown: $event")
@@ -83,11 +125,8 @@ class MainActivity() : AppCompatActivity(), GestureDetector.OnGestureListener,Ge
         return true
     }
 
-    override fun onLongPress(event: MotionEvent) {
-        Log.d(DEBUG_TAG, "onLongPress: $event")
-        Toast.makeText(this, "Long Press", Toast.LENGTH_LONG).show()
 
-    }
+
 
     override fun onScroll(
         event1: MotionEvent,
@@ -109,15 +148,11 @@ class MainActivity() : AppCompatActivity(), GestureDetector.OnGestureListener,Ge
         return true
     }
 
-    override fun onDoubleTap(event: MotionEvent): Boolean {
-        Log.d(DEBUG_TAG, "onDoubleTap: $event")
-        Toast.makeText(this, "Double tap", Toast.LENGTH_LONG).show()
 
-        return true
-    }
 
     override fun onDoubleTapEvent(event: MotionEvent): Boolean {
         Log.d(DEBUG_TAG, "onDoubleTapEvent: $event")
+
         return true
     }
 
@@ -138,19 +173,9 @@ class MainActivity() : AppCompatActivity(), GestureDetector.OnGestureListener,Ge
         return
     }
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event != null) {
-            if (event.values[0] > 5 &&  event.values[1] > 5)
-            {
-                Toast.makeText(this, "ACCELERATION_CHANGE", Toast.LENGTH_LONG).show()
-            }
-        }
-        else {
-            Toast.makeText(this, "Nie wykryto akcelerometra", Toast.LENGTH_LONG).show()
-        }
 
 
-    }
+
 
 
 }
--- Laborki/mobile/app/src/main/java/pl/lipov/laborki/presentation/login/LoginFragment.kt
+++ Laborki/mobile/app/src/main/java/pl/lipov/laborki/presentation/login/LoginFragment.kt
@@ -1,14 +1,23 @@
 package pl.lipov.laborki.presentation.login
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.ColorRes
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
+import androidx.core.content.ContextCompat.getColor
+import androidx.core.graphics.drawable.DrawableCompat.setTint
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import pl.lipov.laborki.R
+import pl.lipov.laborki.data.model.Event
 import pl.lipov.laborki.databinding.FragmentLoginBinding
+import pl.lipov.laborki.presentation.MainActivity
 
 
 class LoginFragment : Fragment() {
@@ -16,10 +25,25 @@ class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
     private var loginCallback: LoginCallback? = null
 
+    private var starAnimator: ValueAnimator? = null
+    private var borderedStarAnimator: ValueAnimator? = null
+
+    lateinit var ACTIVITY: MainActivity
+
+    private val screenUnlockKey =
+        mutableListOf(
+            Event.DOUBLE_TAP.toString(),
+            Event.DOUBLE_TAP.toString(),
+            Event.LONG_CLICK.toString(),
+            Event.ACCELERATION_CHANGE.toString()
+        )
+
     override fun onAttach(
         context: Context
     ) {
         super.onAttach(context)
+        ACTIVITY = context as MainActivity
+
         if (context is LoginCallback) {
             loginCallback = context
         }
@@ -30,7 +54,9 @@ class LoginFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
         return binding.root
     }
 
@@ -38,10 +64,97 @@ class LoginFragment : Fragment() {
         view: View,
         savedInstanceState: Bundle?
     ) {
+
         super.onViewCreated(view, savedInstanceState)
+        var counter: Int = 1
+
+        borderedStarAnimator = binding.icBorderedStar.wrongPasswordAnimation().apply {
+            doOnStart {
+                binding.loginButton.isEnabled = false
+            }
+            doOnEnd {
+                binding.icStar.visibility = View.VISIBLE
+            }
+        }
+
+        starAnimator = binding.icStar.getTintAnimator(duration = 1000).apply {
+            doOnStart {
+                binding.loginButton.isEnabled = false
+            }
+            doOnEnd {
+                // starAnimator?.start()
+                binding.loginButton.isEnabled = true
+            }
+        }
+
+
         binding.loginButton.setOnClickListener {
             loginCallback?.onLoginSuccess()
+        }
+
+        binding.loginButton.setOnClickListener {
+
+            if ((ACTIVITY.loginKeys.elementAtOrNull(0) == screenUnlockKey.elementAtOrNull(0))
+                && (ACTIVITY.loginKeys.elementAtOrNull(1) == screenUnlockKey.elementAtOrNull(1))
+                && (ACTIVITY.loginKeys.elementAtOrNull(2) == screenUnlockKey.elementAtOrNull(2))
+                && (ACTIVITY.loginKeys.elementAtOrNull(3) == screenUnlockKey.elementAtOrNull(3)))
+            {
+                Toast.makeText(context, "Zalogowano", Toast.LENGTH_SHORT).show()
+                starAnimator?.start()
+
+            }
+
+            else if (counter != 3) {
+                borderedStarAnimator?.start()
+                counter = counter?.plus(1)
+                Toast.makeText(context, "Nieudane logowanie", Toast.LENGTH_SHORT).show()
+                binding.loginButton.isEnabled = true
+            }
+            else if (counter == 3) {
+                borderedStarAnimator?.start()
+                Toast.makeText(context, "3 nieudane próby logowania - blokada", Toast.LENGTH_SHORT).show()
+                binding.loginButton.isEnabled = false
+
+            }
+
+        }
+    }
+
+
+    private fun View.getTintAnimator(
+        duration: Long = 500,
+        @ColorRes firstColorResId: Int = R.color.gold,
+        @ColorRes secondColorResId: Int = R.color.grey
+    ): ValueAnimator {
+        return ValueAnimator.ofArgb(
+            getColor(context, firstColorResId),
+            getColor(context, secondColorResId)
+        ).apply {
+            addUpdateListener {
+                setTint(background, it.animatedValue as Int)
+            }
+            this.duration = duration
+        }
+    }
+
+
+
+    private fun View.wrongPasswordAnimation(
+        duration: Long = 500,
+        @ColorRes firstColorResId: Int = R.color.error,
+        @ColorRes secondColorResId: Int = R.color.grey
+    ): ValueAnimator {
+        return ValueAnimator.ofArgb(
+            getColor(context, firstColorResId),
+            getColor(context, secondColorResId)
+        ).apply {
+            addUpdateListener {
+                setTint(background, it.animatedValue as Int)
+            }
+            this.duration = duration
         }
     }
 
 }
+
+
