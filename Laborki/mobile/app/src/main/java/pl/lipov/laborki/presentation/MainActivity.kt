9aa9189bd71cb71216b9a25333122762053112e3
Antoni G <antoni.golos@vp.pl>
2021-04-01 10:36:34 +0200

    zad4 (firebase) - pliki z zajec

--- Laborki/mobile/app/src/main/java/pl/lipov/laborki/presentation/MainActivity.kt
+++ Laborki/mobile/app/src/main/java/pl/lipov/laborki/presentation/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GestureDetectorCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.observe
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import pl.lipov.laborki.R
 import pl.lipov.laborki.data.ViewRouter
@@ -25,6 +28,12 @@ class MainActivity :
     public override fun onCreate(
         savedInstanceState: Bundle?
     ) {
+
+        val database = Firebase.database
+        val myRef = database.getReference("users2")
+
+        myRef.setValue("ff")
+
         super.onCreate(savedInstanceState)
         setTheme(R.style.AppTheme)
         binding = ActivityMainBinding.inflate(layoutInflater)
