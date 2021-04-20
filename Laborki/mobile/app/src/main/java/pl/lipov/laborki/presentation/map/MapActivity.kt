69928c591dd81746d6f78c2501a71d91e20b54fa
KacperSito <kacper.sito@gmail.com>
2021-04-20 23:18:34 +0200

    zad 4 v0.2 (no remove marker feature)

--- Laborki/mobile/app/src/main/java/pl/lipov/laborki/presentation/map/MapActivity.kt
+++ Laborki/mobile/app/src/main/java/pl/lipov/laborki/presentation/map/MapActivity.kt
@@ -1,17 +1,21 @@
 package pl.lipov.laborki.presentation.map
 
+import android.graphics.Point
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import org.koin.android.ext.android.inject
 import pl.lipov.laborki.R
 import pl.lipov.laborki.databinding.ActivityMapBinding
 
-class MapActivity: AppCompatActivity(), OnMapReadyCallback {
+class MapActivity: AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerDragListener {
 
     private lateinit var binding : ActivityMapBinding
     private val mapViewModel by inject<MapViewModel>()
@@ -49,6 +53,11 @@ class MapActivity: AppCompatActivity(), OnMapReadyCallback {
             mapViewModel.setUpMap(googleMap)
 
             setMapLongClick(googleMap)
+
+        with(googleMap){
+            setOnMarkerDragListener(this@MapActivity)
+        }
+
         }
 
 
@@ -78,9 +87,48 @@ class MapActivity: AppCompatActivity(), OnMapReadyCallback {
                     .position(latLng)
                     .title(category)
                     .draggable(true) // allows to drag & drop marker
+
             )
         }
     }
 
 
+
+
+    private fun overlap(point: Point, imgview: ImageView): Boolean {
+        val imgCoords = IntArray(2)
+
+        imgview.getLocationOnScreen(imgCoords)
+        val overlapX =
+            point.x < imgCoords[0] + imgview.width && point.x > imgCoords[0] - imgview.width
+        val overlapY =
+            point.y < imgCoords[1] + imgview.height && point.y > imgCoords[1] - imgview.width
+        return overlapX && overlapY
+    }
+
+
+    override fun onMarkerDragStart(p0: Marker?) {
+        //Toast.makeText(this, "onMarkerDragStart", Toast.LENGTH_SHORT).show()
+
+        // show trash FAB
+        binding.deleteMarkerBtn.visibility=View.VISIBLE
+    }
+
+    override fun onMarkerDrag(p0: Marker?) {
+        //Toast.makeText(this, "onMarkerDrag", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onMarkerDragEnd(marker : Marker) {
+
+// pass somehow map or in other way to overlap function
+//        val markerScreenPosition = map.projection.toScreenLocation(marker.position)
+//        if (overlap(markerScreenPosition, binding.deleteMarkerBtn)){
+//
+//            Toast.makeText(this, "dziala!", Toast.LENGTH_SHORT).show()
+            //marker.remove()
+        
+        println("POS")
+        println(marker.position)
+        binding.deleteMarkerBtn.visibility=View.INVISIBLE
+    }
 }
